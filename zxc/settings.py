ca048f49c6a8745f36819fb3a36e4ac97637f921
chethahernya <povvezzlo@gmail.com>
2022-01-31 18:14:39 +0600

    test

--- zxc/settings.py
+++ zxc/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+
+import os
 from pathlib import Path
 from decouple import config
 
@@ -121,7 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
